59c3f49c165139e8537262bd9b6dd66f2546c151
Lourense <138793950+Louicidio@users.noreply.github.com>
2026-04-12 22:05:07 -0300

    Delete MySQL connection setup
    Remove MySQL connection code from mysql.py

--- MySQL/mysql.py
+++ MySQL/mysql.py
@@ -1,7 +1,1 @@
-import mysql.connector
 
-cnx = mysql.connector.connect(user='root', 
-                              password='Luis74411!',
-                              host='127.0.0.1',
-                              database='Jotair')
-cnx.close()
